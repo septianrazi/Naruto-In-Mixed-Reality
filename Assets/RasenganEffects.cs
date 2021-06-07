77c72d6de023f13a26961c70d2d2a646688cbce3
raz010 <raz010@csiro.au>
2021-06-07 18:54:11 +1000

    Added Rasengan VFX and hand gesture

---
+++ Assets/RasenganEffects.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class RasenganEffects : MonoBehaviour
+{
+    [SerializeField] VisualEffect vfx;
+    [SerializeField] AudioSource startSound;
+    [SerializeField] AudioSource contSound;
+
+    [SerializeField] Vector3 desiredScale = new Vector3(0.07f, 0.07f, 0.07f);
+
+    [SerializeField] RasenganScale rasenganScaleComp;
+
+
+    public void EnableRasengan()
+    {
+        Debug.Log("START RASENGAN");
+        vfx.Play();
+
+        rasenganScaleComp.ChangeToIncRasengan();
+        startSound.volume = 1f;
+        startSound.Play();
+        contSound.volume = 1f;
+        contSound.Play();
+    }
+
+    public void DisableRasengan()
+    {
+        Debug.Log("STopping RASENGAN");
+
+        vfx.Stop();
+
+        rasenganScaleComp.ChangeToUnstableRasengan();
+        StartCoroutine(StartFade(startSound, 2f, 0f));
+        StartCoroutine(StartFade(contSound, 2f, 0f));
+    }
+
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
+            yield return null;
+        }
+        yield break;
+    }
+
+}
---
+++ Assets/RasenganScale.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RasenganScale : MonoBehaviour
+{
+
+    [SerializeField] private bool stableRasengan = false;
+    [SerializeField] private Vector3 stableRasenganScale = new Vector3(0.07f, 0.07f, 0.07f);
+
+    [SerializeField] private Vector3 decrementBy = Vector3.one * 0.00005f;
+    [SerializeField] private Vector3 incrementBy = Vector3.one * 0.001f;
+
+    public bool incRasengan = false;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (incRasengan){
+            IncreaseRasenganSize();
+        }
+        else if (!stableRasengan)
+        {
+            DecreaseRasenganSize(decrementBy);
+        }
+    }
+
+    void DecreaseRasenganSize(Vector3 decrement)
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, 3f * Time.deltaTime);
+
+
+        //if (currentScale.x <= 0.001)
+        //    transform.localScale = Vector3.zero;
+    }
+
+
+    void IncreaseRasenganSize()
+    {
+        transform.localScale = Vector3.Lerp(transform.localScale, stableRasenganScale, 1.5f * Time.deltaTime);
+
+        if (transform.localScale.x >= stableRasenganScale.x)
+        {
+            stableRasengan = true;
+            incRasengan = false;
+        }
+    }
+
+    public void ChangeToUnstableRasengan()
+    {
+        incRasengan = false;
+
+        stableRasengan = false;
+    }
+
+    public void ChangeToIncRasengan()
+    {
+        incRasengan = true;
+    }
+}
