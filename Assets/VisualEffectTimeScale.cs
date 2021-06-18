0a8a1e054cdaaf01d12d9cb3e45069d39dc126a1
raz010 <raz010@csiro.au>
2021-06-18 17:33:11 +1000

    added chidori noise and gesture for chidori

---
+++ Assets/VisualEffectTimeScale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[RequireComponent(typeof(VisualEffect))]
+[ExecuteInEditMode]
+public class VisualEffectTimeScale : MonoBehaviour
+{
+    [Range(0.0f, 10.0f)] public float SimulationTimeScale = 1.0f;
+
+    private VisualEffect Graph;
+
+    private void OnValidate()
+    {
+        Graph = gameObject.GetComponent<VisualEffect>();
+    }
+
+    private void Update()
+    {
+        Graph.playRate = SimulationTimeScale;
+    }
+}
